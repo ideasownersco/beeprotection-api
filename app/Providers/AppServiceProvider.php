b6063254b17a7d6bf2893363df54692487d38135
ZaL <z4ls@live.com>
2019-01-23 14:41:25 +0400

    links

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -18,7 +18,7 @@ class AppServiceProvider extends ServiceProvider
         //
         Schema::defaultStringLength(191);
         if($this->app->environment('production')) {
-            URL::forceScheme('https');
+//            URL::forceScheme('https');
         }
     }
 
