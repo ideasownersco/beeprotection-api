672b4f1c97ec5d40158986ed43d640088eafe948
ZaL <z4ls@live.com>
2019-01-27 14:42:33 +0400

    fix job photos

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -8,8 +8,8 @@ use Fideloper\Proxy\TrustProxies as Middleware;
 class TrustProxies extends Middleware
 {
 //    protected $proxies = ['162.243.164.32'];
-//    protected $proxies = '*';
-    protected $proxies = ['10.136.158.173'];
+    protected $proxies = '*';
+//    protected $proxies = ['10.136.158.173'];
 
     /**
      * The headers that should be used to detect proxies.
